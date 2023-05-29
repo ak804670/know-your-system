bbc1a6e7dde0a4f99b9509dfbf18a5216441f58e
Anish <ak804670@gmail.com>
2023-05-30 03:22:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+const os = require("os")
+const app= express()
+require("dotenv").config()
+
+const port = process.env.PORT
+
+const operatingSystem= os.type()
+app.use("/",(req,res)=>{
+    res.send(`<h1>${operatingSystem}</h1>`)
+})
+
+app.listen(3001,()=>{
+    console.log(`server is up on port${port}`)
+})
+
+
+
+
